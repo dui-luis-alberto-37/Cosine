3e95791a447dfc0e24ae673fdf772b0bcde07345
dui-luis-alberto <lgarciaorozco6@gmail.com>
2023-10-23 12:37:02 -0600

    ejercicio18

--- Cosine.py
+++ Cosine.py
@@ -8,7 +8,13 @@ from math import cos
 x = range(0,1000000,31416)
 x = [i//10000 for i in x]
 y = [cos(i) for i in x]
+x_a = 0
+dx = 3.1416
+for i in range(8):
+    print(x_a)
+    x_a = x_a + dx*i
+    
 
-plt.plot(x,y)
-plt.ylabel('Cosine')
-plt.show()
+# plt.plot(x,y)
+# plt.ylabel('Cosine')
+# plt.show()
